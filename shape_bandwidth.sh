f947069b295f106bb51d9455d405f498ac6ed8ba
Jerome Velociter <jerome@velociter.fr>
2012-03-23 18:12:09 +0100

    FIRST

---
+++ shape_bandwidth.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#
+#  Taken from http://www.topwebhosts.org/tools/tc.bash.txt
+#
+#  tc uses the following units when passed as a parameter.
+#  kbps: Kilobytes per second 
+#  mbps: Megabytes per second
+#  kbit: Kilobits per second
+#  mbit: Megabits per second
+#  bps: Bytes per second 
+#       Amounts of data can be specified in:
+#       kb or k: Kilobytes
+#       mb or m: Megabytes
+#       mbit: Megabits
+#       kbit: Kilobits
+#  To get the byte figure from bits, divide the number by 8 bit
+#
+TC=/usr/sbin/tc
+IF=lo       # Interface 
+DNLD=4mbit          # DOWNLOAD Limt
+UPLD=4mbit       # UPLOAD Limit 
+IP=127.0.0.1     # Host IP
+U32="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
+ 
+start() {
+
+    $TC qdisc add dev $IF root handle 1: htb default 30
+    $TC class add dev $IF parent 1: classid 1:1 htb rate $DNLD
+    $TC class add dev $IF parent 1: classid 1:2 htb rate $UPLD
+    $U32 match ip dst $IP/32 flowid 1:1
+    $U32 match ip src $IP/32 flowid 1:2
+
+}
+
+stop() {
+
+    $TC qdisc del dev $IF root
+
+}
+
+restart() {
+
+    stop
+    sleep 1
+    start
+
+}
+
+show() {
+
+    $TC -s qdisc ls dev $IF
+
+}
+
+case "$1" in
+
+  start)
+
+    echo -n "Starting bandwidth shaping: "
+    start
+    echo "done"
+    ;;
+
+  stop)
+
+    echo -n "Stopping bandwidth shaping: "
+    stop
+    echo "done"
+    ;;
+
+  restart)
+
+    echo -n "Restarting bandwidth shaping: "
+    restart
+    echo "done"
+    ;;
+
+  show)
+                
+    echo "Bandwidth shaping status for $IF:\n"
+    show
+    echo ""
+    ;;
+
+  *)
+
+    pwd=$(pwd)
+    echo "Usage: $(/usr/bin/dirname $pwd)/tc.bash {start|stop|restart|show}"
+    ;;
+
+esac
+
+exit 0
+
